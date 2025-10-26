20769640c87e3f642ee5370c1b7daa893ea02ac9
miguelorg12 <miguelvillalpando19@gmail.com>
2025-10-26 01:08:45 -0600

    feat: :sparkles: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import routes from "./routes";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use("/api", routes);
